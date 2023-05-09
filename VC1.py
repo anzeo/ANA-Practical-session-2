e50e554cb456bd9eac0b4da20b0441de9b450ac7
Anže Oblak <anze.oblak@rettro.si>
2023-05-09 17:05:01 +0200

    Greedy approach for vertex cover problem

--- VC1.py
+++ VC1.py
@@ -13,14 +13,13 @@ def vc_naive_approach(G):
     C = set()
 
     uncovered_edges = set(G.edges)
-    # tmp_G = G.copy()  # Make a copy of graph so the original isn't changed
 
     while uncovered_edges:
         (u, v) = uncovered_edges.pop()
         if u not in C and v not in C:
             C.add(u)
 
-        # Is it worth doing that?
+        # A je to optimalno delat?
         # for adjacent_edge in list(tmp_G.edges(u)):
         #     # print(adjacent_edge)
         #     (au, av) = adjacent_edge
@@ -36,25 +35,98 @@ def vc_naive_approach(G):
 def vc_greedy_approach(G):
     C = set()
 
+    uncovered_edges = set(G.edges)
+
+    tmp_G = G.copy()  # Make a copy of graph so the original isn't changed
+
+    # start = time.time()
+    vertices = dict()
+    for vertex in G.nodes:
+        # vertices[vertex] = len(G.edges(vertex))
+        adj_edge_count = len(G.edges(vertex))
+        if adj_edge_count not in vertices:
+            vertices[adj_edge_count] = set()
+        vertices[adj_edge_count].add(vertex)
+    # end = time.time()
+    # print("Vertex time: ", end - start)
+    # print(max(vertices))
+    """
+
+        1----2----3
+        |    |
+        |    |
+        4----5----6
+    """
+    # totalMax = 0.0
+    # totalAdjacent = 0.0
+    # start = time.time()
+    while uncovered_edges:
+        # start1 = time.time()
+
+        max_adj_edge_count = max(vertices)  # Find set of vertices with most uncovered adjacent edges.
+        max_vertex = vertices[max_adj_edge_count].pop()  # Select one of vertices with most uncovered adjacent edges.
+        if not vertices[max_adj_edge_count]:
+            del vertices[max_adj_edge_count]
+        # end1 = time.time()
+        # totalMax += (end1 - start1)
+
+        # print(max_vertex)
+        C.add(max_vertex)
+
+        # start1 = time.time()
+        for adjacent_edge in list(tmp_G.edges(max_vertex)):
+            (u, v) = adjacent_edge
+            adj_vertex = (u if u != max_vertex else v)
+            adj_vertex_degree = len(tmp_G.edges(adj_vertex))
+            vertices[adj_vertex_degree].remove(adj_vertex)
+            if adj_vertex_degree > 1:
+                if adj_vertex_degree - 1 not in vertices:
+                    vertices[adj_vertex_degree - 1] = set()
+                vertices[adj_vertex_degree - 1].add(adj_vertex)
+            if not vertices[adj_vertex_degree]:
+                del vertices[adj_vertex_degree]
+
+            # vertices[u] -= 1
+            # vertices[v] -= 1
+
+            if (u, v) in uncovered_edges:
+                uncovered_edges.remove((u, v))
+            if (v, u) in uncovered_edges:
+                uncovered_edges.remove((v, u))
+            tmp_G.remove_edge(u, v)
+
+        # del vertices[max_vertex]
+        # end1 = time.time()
+        # totalAdjacent += (end1 - start1)
+
+    # end = time.time()
+    # print("Cover search time: ", end - start)
+    # print("Total max time: ", totalMax)
+    # print("Total adjacent time: ", totalAdjacent)
+
+    return C
+
+
     return C
 
 
 if __name__ == '__main__':
     directory = 'tests'
-    # start = time.time()
-    # for filename in os.listdir(directory):
-    #     f = os.path.join(directory, filename)
-    #     # checking if it is a file
-    #     if os.path.isfile(f):
-    #         fo = open(f, "rb")
-    #         G = nx.read_edgelist(fo)
-    #         fo.close()
-    #         a = nx.maximal_matching(G)
-    #         b = vc_naive_approach(G)
-    #         print(filename, " ", str(len(a)))
-    #     # break
-    # end = time.time()
-    # print(end - start)
+    start = time.time()
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            fo = open(f, "rb")
+            G = nx.read_edgelist(fo)
+            fo.close()
+            a = nx.maximal_matching(G)
+            naive = vc_naive_approach(G)
+            greedy = vc_greedy_approach(G)
+            print(filename, " ", str(len(naive)), str(len(greedy)))
+        # break
+    end = time.time()
+    print(end - start)
 
     """
          7
@@ -66,13 +138,19 @@ if __name__ == '__main__':
     4----5
     """
 
-    filename = "tests/test.graph"
-    f = open(filename, "rb")
-    G = nx.read_edgelist(f, nodetype=int)
-    f.close()
-    # print(G.edges())
-    # print(nx.maximal_matching(G))
-    print(vc_naive_approach(G))
-    # print(len(vc_greedy_approach(G)))
-    # print(G.edges)
+    """
 
+        1----2----3
+        |    |
+        |    |
+        4----5
+    """
+
+    # filename = "tests/test.graph"
+    # f = open(filename, "rb")
+    # G = nx.read_edgelist(f, nodetype=int)
+    # f.close()
+    # print(vc_naive_approach(G))
+    # print((vc_greedy_approach(G)))
+
+    # print(G.edges)
